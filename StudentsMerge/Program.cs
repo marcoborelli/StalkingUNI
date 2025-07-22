f239e14dca94df158a76f556fe5dd41a67fe8473
marcoborelli <marco05.borelli@gmail.com>
2025-07-22 14:09:14 +0200

    Rifatta la gestione degli indici di args tramite enum

--- StudentsMerge/Program.cs
+++ StudentsMerge/Program.cs
@@ -4,21 +4,33 @@ using System.Collections.Generic;
 
 namespace StudentsMerge {
     class MainClass {
+        public enum Params {
+            OrigFilename,
+            NewDataFilename,
+            Count
+        }
+
         public static void Main(string[] args) {
-            if (args.Length != 2) {
-                Console.WriteLine("Error. Usage: ./programma <OrigFilename> <NewDataFilename>");
+            if (args.Length != (int)Params.Count) {
+                Console.Write($"Error. Usage: ./programma ");
+                for (byte i = 0; i < (byte)Params.Count; i++) {
+                    Console.Write($"<{Enum.GetName(typeof(Params), i)}> ");
+                }
+                Console.Write("\n");
+
+
                 throw new Exception("Wrong number of parameters");
             }
 
 
-            if (!File.Exists(args[1])) {
-                throw new Exception("Wrong input file path");
+            if (!File.Exists(args[(int)Params.NewDataFilename])) {
+                throw new Exception("Wrong new file path");
             }
 
             //se non esiste il file originale lo si crea da zero, restituisco solo un warning
-            if (!File.Exists(args[0])) {
-                Console.WriteLine($"Il file {args[0]} non esiste. Ne verra' creato uno nuovo");
-                File.Create(args[0]).Close(); //close in quanto senno' resterebbe aperto uno stream creando poi eccezione
+            if (!File.Exists(args[(int)Params.OrigFilename])) {
+                Console.WriteLine($"Il file {args[(int)Params.OrigFilename]} non esiste. Ne verra' creato uno nuovo");
+                File.Create(args[(int)Params.OrigFilename]).Close(); //close in quanto senno' resterebbe aperto uno stream creando poi eccezione
             }
 
             Dictionary<int, string> mat_cognomeNome = new Dictionary<int, string>();
@@ -26,7 +38,7 @@ namespace StudentsMerge {
 
             //salvo in un dizionario tutti i nuovi valori del file
             string line = "";
-            using (StreamReader sr = new StreamReader(args[1])) {
+            using (StreamReader sr = new StreamReader(args[(int)Params.NewDataFilename])) {
                 sr.ReadLine(); //salto l'intestazione
                 while ((line = sr.ReadLine()) != null) {
                     string[] fields = line.Split(';');
@@ -38,7 +50,7 @@ namespace StudentsMerge {
 
 
             //rimuovo dal dizionario i valori che gia' esistono all'interno del file originale
-            using (StreamReader sr = new StreamReader(args[0])) {
+            using (StreamReader sr = new StreamReader(args[(int)Params.OrigFilename])) {
                 while ((line = sr.ReadLine()) != null) {
                     string[] fields = line.Split(';');
                     int mat = int.Parse(fields[0]);
@@ -50,7 +62,7 @@ namespace StudentsMerge {
 
 
             //i valori restanti nel dizionario sono quelli da aggiungere in append al file originale
-            using (StreamWriter sw = new StreamWriter(args[0], true)) {
+            using (StreamWriter sw = new StreamWriter(args[(int)Params.OrigFilename], true)) {
                 foreach (KeyValuePair<int, string> kvp in mat_cognomeNome) {
                     sw.WriteLine($"{kvp.Key};{kvp.Value}");
                 }
