b0763499478e72b8ba4a4ce8500cec8a1f3b8ecd
marcoborelli <marco05.borelli@gmail.com>
2025-07-29 16:39:06 +0200

    StalkingUNIBackend: aggiunta funzione che restituisce tutti i voti presi da una matricola agli esami

--- StalkingUNIBackend/CSVManager/CSVManager.go
+++ StalkingUNIBackend/CSVManager/CSVManager.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -81,6 +82,54 @@ func GetMatricola(filepath, cognome, nome string) (studenti []Studente) {
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println(err)
+	}
+
+	return
+}
+
+func GetVoti(dirpath, matricola string) (voti map[string]int) {
+	data, err := (os.ReadDir(dirpath)) //la cartella letta contiene sottocartelle, ognuna per ogni materia
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	voti = make(map[string]int)
+
+	for _, elem := range data {
+		//ogni sottocaretella contiene un file .csv con lo stesso nome della directory in cui e' contenuto
+		file, err := os.Open(dirpath + elem.Name() + string(os.PathSeparator) + elem.Name() + ".csv")
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+
+		for scanner.Scan() {
+			line := scanner.Text()
+
+			fields := strings.Split(line, ";")
+
+			if fields[0] == matricola {
+				ris, err := strconv.Atoi(fields[1])
+				if err != nil {
+					fmt.Println(err)
+					voti[elem.Name()] = -1
+				} else {
+					voti[elem.Name()] = ris
+				}
+				break
+			}
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Println(err)
+		} else if _, ok := voti[elem.Name()]; !ok {
+			//se sono qui non esiste la chiave -> l'esame non e' stato sostenuto
+			voti[elem.Name()] = -1
+		}
 	}
 
 	return
