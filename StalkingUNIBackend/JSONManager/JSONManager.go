9cc812bc5ba28daef84e77f1f0ab651aa053d1c7
marcoborelli <marco05.borelli@gmail.com>
2025-07-29 23:35:09 +0200

    StalkingUNIBackend: aggiunto pacchetto JSONManager per la creazione di stringhe json da inviare come risposta nelle future API

---
+++ StalkingUNIBackend/JSONManager/JSONManager.go
@@ -0,0 +1,32 @@
+package JSONManager
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/marcoborelli/StalkingUNI/CSVManager"
+)
+
+func StructUserToJSON(input CSVManager.Studente) (res string) {
+	bytes, err := json.Marshal(input)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	res = string(bytes[:])
+	fmt.Println(res)
+	return
+}
+
+func MapEsamiToJSON(input map[string]int) (res string) {
+	bytes, err := json.Marshal(input)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	res = string(bytes[:])
+	fmt.Println(res)
+	return
+}
