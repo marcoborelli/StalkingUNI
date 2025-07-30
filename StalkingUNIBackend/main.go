44720e2743d763008d27b78a28c8f907b816fab6
marcoborelli <marco05.borelli@gmail.com>
2025-07-30 17:21:09 +0200

    StalkingUNIBackend: implementato main

--- StalkingUNIBackend/main.go
+++ StalkingUNIBackend/main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/joho/godotenv"
+	"github.com/marcoborelli/StalkingUNI/ServerHTTP"
 )
 
 func main() {
 	fmt.Println("Helo")
+
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	ServerHTTP.StartServer(":8080")
 }
