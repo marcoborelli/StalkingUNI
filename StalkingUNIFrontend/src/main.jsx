8c4d1171f22bf35c8507af08792457d43ccd639a
marcoborelli <marco05.borelli@gmail.com>
2025-09-12 23:53:54 +0200

    StalkingUNIFrontend: creato routing delle pagine

--- StalkingUNIFrontend/src/main.jsx
+++ StalkingUNIFrontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import PageSearch from './pages/PageSearch.jsx'
+import PageStudent from './pages/PageStudent.jsx'
+
+const router = createBrowserRouter([
+  { path: '/', element: <PageSearch /> },
+  { path: '/user/:matricola', element: <PageStudent /> },
+  { path: '*', element: <PageSearch /> }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
 )
