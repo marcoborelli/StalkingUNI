e3c39d5ccfbfa18e603c6540ad7c4e590280dd7a
marcoborelli <marco05.borelli@gmail.com>
2025-09-13 22:33:58 +0200

    StalkingUNIFrontend: aggiunto basePath e porta al file di configurazione di vite

--- StalkingUNIFrontend/vite.config.js
+++ StalkingUNIFrontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+
+export default defineConfig(() => {
+  const env = loadEnv('', './', '')
+  return {
+    plugins: [react()],
+    base: env.VITE_PUBLIC_URL_BASE_PATH,
+    server: {
+      host: '0.0.0.0',
+      port: parseInt(env.VITE_PORT)
+    }
+  }
 })
