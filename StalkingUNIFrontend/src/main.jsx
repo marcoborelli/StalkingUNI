dd8867debefd95cf0d6970dc5eb6709a6f578b29
marcoborelli <marco05.borelli@gmail.com>
2025-09-13 22:34:15 +0200

    StalkingUNIFrontend: implementato basePath anche nel router

--- StalkingUNIFrontend/src/main.jsx
+++ StalkingUNIFrontend/src/main.jsx
@@ -9,7 +9,9 @@ const router = createBrowserRouter([
   { path: '/', element: <PageSearch /> },
   { path: '/user/:matricola', element: <PageStudent /> },
   { path: '*', element: <PageSearch /> }
-])
+],
+  { basename: import.meta.env.BASE_URL }
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
